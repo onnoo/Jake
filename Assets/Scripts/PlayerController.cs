68c7d3e6d2895fe71a6ed70c6291807c7c2ae78f
onnoo <sweyjw@gmail.com>
2018-09-26 03:55:58 +0900

    Commit Chap09

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce;
+    private Rigidbody2D rigidBody;
+    public Animator animator;
+    public float runningSpeed = 1.5f;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        animator.SetBool("isAlive", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
+        animator.SetBool("isGrounded", IsGrounded());
+    }
+
+    private void FixedUpdate()
+    {
+        if (rigidBody.velocity.x < runningSpeed)
+        {
+            rigidBody.velocity = new Vector2(runningSpeed, rigidBody.velocity.y);
+        }
+    }
+
+    void Jump()
+    {
+        if (IsGrounded())
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    public LayerMask groundLayer;
+
+    bool IsGrounded()
+    {
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, 0.2f, groundLayer.value))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
